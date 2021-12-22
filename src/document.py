d95f16b91c784d1744c79613ad3ff6c76dd69373
aymenkhs <aymenkhouas@gmail.com>
2021-12-23 00:11:28 +0100

    tokenisation

--- src/document.py
+++ src/document.py
@@ -1,7 +1,7 @@
 import nltk
+from nltk.corpus import stopwords
 
 class Document:
-
 	def __init__(self, id, title, abstract):
 
 		self.id = id
@@ -10,17 +10,15 @@ class Document:
 
 	def tokenise(self):
 		self.words = nltk.word_tokenize(self.title + ' ' + self.abstract)
-	    words = [word.lower() for word in words if word.isalnum()]
+		self.words = [word.lower() for word in self.words if word.isalnum()]
 
-	    self.frequency = {}
-	    for word in self.words:
-	        if word in stopwords.words('english') or word.isspace() or word == "":
-	            continue
+		self.frequency = {}
+		for word in self.words:
+			if word in stopwords.words('english') or word.isspace() or word == "":
+				continue
+			if word in self.frequency:
+				self.frequency[word] += 1
+			else:
+				self.frequency[word] = 1
 
-	        if word in document:
-	            self.frequency[word] += 1
-	        else:
-	            self.frequency[word] = 1
-
-
-	    return self.frequency
+		return self.frequency
