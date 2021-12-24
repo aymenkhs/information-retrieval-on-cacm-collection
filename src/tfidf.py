311397df79eebb9859d02fa42e4ad2083df388b4
aymenkhs <aymenkhouas@gmail.com>
2021-12-24 16:40:49 +0100

    Changing the project structure
    - adding modules tfidf, verctoriel_model, boolean_model
    - transfering main content to tfidf module

---
+++ src/tfidf.py
@@ -0,0 +1,34 @@
+import math
+
+from src.read_file import *
+from src.document import Document
+
+def indexation():
+	Document.STOPWORDS = read_stop_list()
+	read_docs(read_file(DOCUMENT_FILE_PATH))
+	for document in Document.DOCUMENTS:
+		document.tokenise()
+	return Document.DOCUMENTS
+
+def inverse_file():
+	words = Document.all_words()
+	inverse_structure = {}
+	for word in words:
+		inverse_structure[word] = []
+		for document in Document.DOCUMENTS:
+			if word in document.words:
+				inverse_structure[word].append((document, document.frequency[word]))
+	return inverse_structure
+
+def tf_idf(inverse_structure):
+	weight = lambda ti, dj: (dj.frequency[ti]/dj.max_frequency()) * math.log10(Document.nb_documents()/len(inverse_structure[ti]) + 1)
+
+	words = Document.all_words()
+
+	inverse_weight_matrix = {}
+	for word in words:
+		inverse_weight_matrix[word] = []
+		for document in Document.DOCUMENTS:
+			if word in document.words:
+				inverse_weight_matrix[word].append((document, weight(word, document)))
+	return inverse_weight_matrix
---
+++ src/verctoriel_model.py
@@ -0,0 +1,1 @@
+from src.document import Document
