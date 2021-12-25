9da565d5c54c1f72a6ae5a9417c67596d4b2137b
aymenkhs <aymenkhouas@gmail.com>
2021-12-25 19:08:54 +0100

    writing an evaluation function for a single instance

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from src.tfidf import execute_tfidf
 from src.boolean_model import boolean_model
 from src.vectorial_model import vectorial_model, LIST_MEASURES_FUNCTIONS
 from src.pkl import load_results
-from src.evaluations import launch_all_evaluations
+from src.evaluations import launch_all_evaluations, evaluate_vectorial_function
 from src.read_file import read_requests
+from src.requests import Request
 
 
 def parse_arguments():
@@ -27,7 +28,7 @@ def main():
 	if args.evaluations:
 		launch_all_evaluations(inverse_weight_matrix)
 	elif args.test:
-		pass
+		print(evaluate_vectorial_function(inverse_weight_matrix, 'cosinus measure', Request.REQUESTS[0], 0.2))
 	else:
 		pass
 		# launch the UI
--- src/evaluations.py
+++ src/evaluations.py
@@ -12,41 +12,36 @@ calculate_precision = lambda docs_pert_select, docs_select : docs_pert_select / 
 calculate_recall = lambda docs_pert_select, docs_pert : docs_pert_select / docs_pert
 
 
+class EvaluationError(Exception):
+	def __init__(self):
+		self.message = 'There are no expected documents for this query'
+
+	def __str__(self):
+		return self.message
+
 def evaluate(selected_documents, request):
 	 docs_pert_select = [document for document in selected_documents if document.id in request.expected_results]
 	 recall = calculate_precision(len(docs_pert_select), len(request.expected_results))
 	 precision = calculate_precision(len(docs_pert_select), len(selected_documents))
 	 return precision, recall
 
-def evaluate_vectorial_function(inverse_weight_matrix, function):
-	read_requests()
-	tresholds = [x/10 for x in list(range(1,6))]
+def evaluate_vectorial_function(inverse_weight_matrix, function, request, treshold):
 
-	list_results = []
-	for request in Request.REQUESTS:
-		if len(request.expected_results) == 0:
-			continue
-
-		for treshold in tresholds:
-			selected_documents = vectorial_model(request.content,
-				inverse_weight_matrix, LIST_MEASURES_FUNCTIONS[function], threshold=treshold)
-			if len(selected_documents) == 0:
-				precision, recall, fmeasure = 0, 0, 0
-			else :
-				precision, recall = evaluate(selected_documents, request)
-				if precision == 0 and recall == 0:
-					fmeasure = 0
-				else:
-					fmeasure = calculate_fmeasure(precision, recall)
-
-			print(request.id, treshold, precision, recall, fmeasure)
-			df2 = [request.id, treshold, precision, recall, fmeasure]
-			list_results.append(df2)
-	evaluations_results = pd.DataFrame(list_results, columns=['request', 'treshold', 'precision', 'recall', 'fmeasure'])
-	evaluations_results.to_csv(os.path.join(RESULT_FOLDER, '{}.csv'.format(function)))
+	if len(request.expected_results) == 0:
+		raise EvaluationError()
+	selected_documents = vectorial_model(request.content,
+		inverse_weight_matrix, LIST_MEASURES_FUNCTIONS[function], threshold=treshold)
+	if len(selected_documents) == 0:
+		precision, recall, fmeasure = 0, 0, 0
+	else :
+		precision, recall = evaluate(selected_documents, request)
+		if precision == 0 and recall == 0:
+			fmeasure = 0
+		else:
+			fmeasure = calculate_fmeasure(precision, recall)
+	return precision, recall, fmeasure, selected_documents
 
 def evaluate_and_store(inverse_weight_matrix, function):
-
 	tresholds = [x/10 for x in list(range(1,6))]
 
 	list_results = []
