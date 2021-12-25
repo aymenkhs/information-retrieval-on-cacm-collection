71b1b06b422a6eaf6b0b9cb4f30a9f8be6ad6e19
aymenkhs <aymenkhouas@gmail.com>
2021-12-25 11:04:13 +0100

    Save the inverse_weight_matrix, inverse_structure and the document lists in pkl files to save execution time

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from src.tfidf import indexation, inverse_file, tf_idf
 from src.boolean_model import boolean_model
-from src.verctoriel_model import vectorial_model, LIST_MEASURES_FUNCTIONS
+from src.vectorial_modal import vectorial_model, LIST_MEASURES_FUNCTIONS
+from src.pkl import save_results, load_results
 
-def main():
+def execute_tfidf():
 	indexation()
 	inverse_structure = inverse_file()
 	inverse_weight_matrix = tf_idf(inverse_structure)
+	save_results(inverse_weight_matrix, inverse_structure)
+	return inverse_weight_matrix, inverse_structure
+
+def main():
+	inverse_weight_matrix, inverse_structure = load_results()
 	string = 'What articles exist which deal with TSS (Time Sharing System), an \n operating system for IBM computers?'
 	print(vectorial_model(string, inverse_weight_matrix, LIST_MEASURES_FUNCTIONS['cosinus measure']))
 
