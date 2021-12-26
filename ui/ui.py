0f9285374b487bb8d7297485f698c76f3fc4aac2
aymenkhs <aymenkhouas@gmail.com>
2021-12-26 04:49:53 +0100

    Working on the UI
    - imported all the stuff we need in the ui
    - called the ui from main
    - calling the boolean method
    - checking if the returned value is an empty list
    - adding selected documents to the list
    - clearing the list before new asignement and if the request failed

--- ui/ui.py
+++ ui/ui.py
@@ -5,12 +5,34 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.uic import loadUi
 
-from main_interface import Ui_MainWindow
+from ui.main_interface import Ui_MainWindow
+
+from src.pkl import load_results
+from src.read_file import read_requests
+from src.requests import Request
+from src.evaluations import launch_all_evaluations, evaluate_vectorial_function
+
+from src.tfidf import execute_tfidf
+from src.boolean_model import boolean_model
+from src.vectorial_model import vectorial_model, LIST_MEASURES_FUNCTIONS
+
 
 class Window(QMainWindow, Ui_MainWindow):
 	def __init__(self, parent=None):
 		super().__init__(parent)
 		self.setupUi(self)
+
+		self.setup_values()
+		self.setup_conections()
+
+	def setup_values(self):
+		read_requests()
+		self.inverse_weight_matrix, self.inverse_structure = load_results()
+		self.QueryText.setPlainText('inverse_structure')
+
+	def setup_conections(self):
+		self.SearchInput.clicked.connect(self.search_by_input)
+		self.SearchQueryText.clicked.connect(self.search_by_query)
 
 	def search_by_query(self):
 		if self.ModelChoice.currentText() == 'Le Model Boolean':
@@ -20,9 +42,7 @@ class Window(QMainWindow, Ui_MainWindow):
 			pass
 
 	def search_by_input(self):
-
-		query = self.QueryInput.text()
-
+		query = self.QueryInput.toPlainText()
 		if query == "":
 			QMessageBox.about(self, "the query must not be empty",
 			"please write your query before clicking the search button")
@@ -30,14 +50,36 @@ class Window(QMainWindow, Ui_MainWindow):
 
 		if self.ModelChoice.currentText() == 'Le Model Boolean':
 			# call the boolean model
-			pass
+			selected_documents = boolean_model(query)
 		else:
 			# call the vectoriel model
 			pass
 
 
-if __name__ == "__main__":
+		if not self.is_selection_empty(selected_documents):
+			# show the content inside the list
+			self.update_list(selected_documents)
+
+	def is_selection_empty(self, selected_documents):
+		if len(selected_documents) == 0:
+			QMessageBox.about(self, "no document found",
+			"No document has been found with this query and parameters")
+			self.Result.clear()
+			return True
+		return False
+
+	def update_list(self, selected_documents):
+		# clear the list
+		self.Result.clear()
+		# add items
+		for document in selected_documents:
+			self.Result.addItem(document.id)
+
+def launch_ui():
 	app = QApplication(sys.argv)
 	window = Window()
 	window.show()
 	sys.exit(app.exec_())
+
+if __name__ == "__main__":
+	launch_ui()
