e5047341ca765413ac5fdc7aafe6b77009a71c2c
aymenkhs <aymenkhouas@gmail.com>
2022-01-04 12:48:18 +0100

    fixing the first execution (pkl not loaded)

--- ui/ui.py
+++ ui/ui.py
@@ -36,7 +36,11 @@ class Window(QMainWindow, Ui_MainWindow):
 
 	def setup_values(self):
 		read_requests()
-		self.inverse_weight_matrix, self.inverse_structure = execute_tfidf()
+		try:
+			self.inverse_weight_matrix, self.inverse_structure = load_results()
+		except FileNotFoundError as e:
+			self.inverse_weight_matrix, self.inverse_structure = execute_tfidf()
+
 		self.QueryText.setPlainText(Request.REQUESTS[0].content)
 
 	def setup_conections(self):
